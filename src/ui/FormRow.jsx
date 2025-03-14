1d018d869bfda6e156be8dce570056f536e02c61
mdrgoncalves <mdr.goncalves@hotmail.com>
2025-03-14 13:09:59 -0300

    style: adjust StyledFormRow layout to improve responsiveness

--- src/ui/FormRow.jsx
+++ src/ui/FormRow.jsx
@@ -3,8 +3,8 @@ import styled from "styled-components";
 const StyledFormRow = styled.div`
   display: grid;
   align-items: center;
-  grid-template-columns: 24rem 1fr 1.2fr;
-  gap: 2.4rem;
+  grid-template-columns: 1 1fr 1.2fr;
+  gap: 1rem;
 
   padding: 1.2rem 0;
 
@@ -16,9 +16,10 @@ const StyledFormRow = styled.div`
     padding-bottom: 0;
   }
 
+  /* 
   &:not(:last-child) {
     border-bottom: 1px solid var(--color-grey-100);
-  }
+  } */
 
   &:has(button) {
     display: flex;
