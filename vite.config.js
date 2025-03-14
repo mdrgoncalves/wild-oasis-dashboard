a032747562331f32dfcd115f431cc94e9ae4a308
mdrgoncalves <mdr.goncalves@hotmail.com>
2025-03-14 12:59:38 -0300

    feat: add Supabase config and update .gitignore

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), eslint()],
-});
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return defineConfig({
+    plugins: [react(), eslint()],
+    define: {
+      "process.env.SUPABASE_URL": env.SUPABASE_URL,
+      "process.env.SUPABASE_KEY": env.SUPABASE_KEY,
+    },
+  });
+};
