f4b93359645dfb2ef067bdd96e6b8ea81de1f94f
mdrgoncalves <mdr.goncalves@hotmail.com>
2025-03-14 12:59:46 -0300

    feat: implement authentication flow with login and logout functionality

--- src/ui/ProtectedRoute.jsx
+++ src/ui/ProtectedRoute.jsx
@@ -1,12 +1,39 @@
+import { useEffect } from "react";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+
+import { useUser } from "../features/authentication/useUser";
+import Spinner from "./Spinner";
+
+const FullPage = styled.div`
+  height: 100vh;
+  background-color: var(--color-grey-50);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
 function ProtectedRoute({ children }) {
+  const navigate = useNavigate();
+
   // 1. Load the authenticated user
+  const { isLoading, isAuthenticated } = useUser();
 
-  // 2. While loading, show a spinner
+  // 2. If there is NO authenticated user, redirect to the /login
+  useEffect(() => {
+    if (!isAuthenticated && !isLoading) navigate("/login");
+  }, [isAuthenticated, isLoading, navigate]);
 
-  // 3. If there is NO authenticated user, redirect to the /login
+  // 3. While loading, show a spinner
+  if (isLoading)
+    return (
+      <FullPage>
+        <Spinner />
+      </FullPage>
+    );
 
   // 4. If there is a user, render the app
-  return children;
+  if (isAuthenticated) return children;
 }
 
 export default ProtectedRoute;
